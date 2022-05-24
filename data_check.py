65f877df565db5da8587a350d10981245dd230b3
Decorise Major <89880049+dmajorky@users.noreply.github.com>
2022-05-23 22:58:21 -0400

    Add files via upload

---
+++ data_check.py
@@ -0,0 +1,10 @@
+print("hello world!")
+
+cars = ['Bugatti', 'Bentley', 'Porsche', 'Acura',]
+print(cars[3])
+
+cars = {'Bugatti' : 'Exotic', 'Bentley' : 'Ultra luxury', 'Porsche' : 'Luxury', 'Acura' : 'mid-level'}
+print(cars['Bentley'])
+
+car price = ['very very expensive', 'very expensive', 'expensive', 'affordable']
+print(car price[1])
